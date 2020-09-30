ca31c7a523b7061ce44257d396dc0efce72e0256
liamH47 <liamhanafee@gmail.com>
2020-09-30 13:53:36 -0400

    first few seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,9 @@ require 'rest-client' # actually let you hit the endpoint
 require 'json' # parse the response body into a form you can use 
 require 'pry'
 
-user1 = User.new
-
-
+User.create({ name: "Javaughn" })
+User.create({ name: "Liam" })
+Enemy.create({ name: "Cell", health: 120})
+Goku.create ({ user_id: 1, health: 100, honor: 4})
+Battle.create ({ name: "Fight Cell!", goku_id: 1, user_id: 1, enemy_id: 2})
 # binding.pry
