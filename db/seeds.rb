71b2f10c042a446501acb45578a40d5a1d381448
liamH47 <liamhanafee@gmail.com>
2020-09-30 14:21:27 -0400

    pretty much same

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,8 @@ require 'pry'
 
 User.create({ name: "Javaughn" })
 User.create({ name: "Liam" })
-Enemy.create({ name: "Cell", health: 120})
-Goku.create ({ user_id: 1, health: 100, honor: 4})
-Battle.create ({ name: "Fight Cell!", goku_id: 1, user_id: 1, enemy_id: 2})
+Enemy.create({ name: "Cell", health: 120 })
+Goku.create ({ user_id: 1, health: 100, honor: 4 })
+Goku.create ({ user_id: 1, health: 100, honor: 4 })
+Battle.create ({ name: "Fight Cell!", goku_id: 1, user_id: 1, enemy_id: 2 })
 # binding.pry
